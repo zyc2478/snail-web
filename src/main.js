90cb4ac6c60f695f4ee2f8afad92f4d961e32ac9
Richard Zeng <zyc2478@126.com>
2018-03-14 17:27:45 +0800

    引入ElementUI，代码调试通过，可以前后端分离方式，正常登陆

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import iView from 'iview';
+import VueRouter from 'vue-router';
+import router from './router';
+import App from './app.vue';
+import 'iview/dist/styles/iview.css';
+import axios from './axios';
+import VueAxios from 'vue-axios';
+import store from './store/store';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+Vue.use(VueAxios,axios);
+
+Vue.use(iView);
+
+new Vue({
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => h(App)
+});
